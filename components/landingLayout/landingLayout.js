4ab7584ccb1030b4482d94046b591e9c51c893bc
Abhishek Chhabra <abhishekchhabra107@gmail.com>
2022-07-25 19:24:58 +0530

    joey: resolve build errors

--- components/landingLayout/landingLayout.js
+++ components/landingLayout/landingLayout.js
@@ -20,7 +20,7 @@ const LandingLayout = () => {
                 <div className={landingLayoutStyle.greetingsContainer}>
                     <h1 className={`${landingLayoutStyle.greetingsText} ${utilStyles.textAlignLeft} ${utilStyles.regularMarginTop}`}>Hi, I&apos;m Joey.</h1>
                     <h2 className={`${landingLayoutStyle.introductoryText} ${utilStyles.textAlignLeft} ${utilStyles.regularMarginTop}`}>Your personal guide in the world of BITS Pilani</h2>
-                    <h4 className={`${landingLayoutStyle.descriptionText} ${utilStyles.textAlignLeft} ${utilStyles.regularMarginTop}`}>I've been made to make your life easier, you can come and ask me questions about your semesters, subjects, books, useful links etc. and I&apos;ll try my best to guide you through it.</h4>
+                    <h4 className={`${landingLayoutStyle.descriptionText} ${utilStyles.textAlignLeft} ${utilStyles.regularMarginTop}`}>I&apos;ve been made to make your life easier, you can come and ask me questions about your semesters, subjects, books, useful links etc. and I&apos;ll try my best to guide you through it.</h4>
                     <div className={landingLayoutStyle.buttonContainer}>
                         <ButtonLayout buttonStylesAndInfo={{buttonText: "E-Books Library", buttonTextCode: "ebooks", backgroundColor: "#FF500A", color: "white"}} handleButtonClick={handleButtonClick} />
                         <ButtonLayout buttonStylesAndInfo={{buttonText: "E-Learn Portal", buttonTextCode: "elearn", backgroundColor: "#FF500A", color: "white", buttonAdditionalStyle: "margin-left: 1rem"}} handleButtonClick={handleButtonClick} />
